9a0193c0d3208adce5a3b69e8427bae5ba3f1bd7
btipton <btipton@spectrumsedge.com>
2015-09-09 15:29:52 -0400

    Created DriveTrainCommand class and a test opmode

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ProgramTestOp.java
@@ -0,0 +1,27 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.ftcrobotcontroller.DrivetrainCommand;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by Bob on 9/9/2015.
+ */
+public class ProgramTestOp extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+        DrivetrainCommand driveCommand;
+
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}

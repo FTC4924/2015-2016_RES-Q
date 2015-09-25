7585c75f7630ec272efc48cbb6e45df9039640a4
btipton <btipton@spectrumsedge.com>
2015-09-24 21:56:27 -0400

    Added scaling to driving and bucket arm moving.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/BucketPowerCalculator.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/BucketPowerCalculator.java
@@ -1,5 +1,7 @@
 package com.qualcomm.ftcrobotcontroller;
 
+import com.qualcomm.robotcore.util.Range;
+
 /**
  * Created by 4924_Users on 9/20/2015.
  */
@@ -9,8 +11,41 @@ public class BucketPowerCalculator {
         float power;
         power = inputs.throttle;
 
+        power = Range.clip(power, -1, 1);
+        power = (float)scaleInput(power);
+
         levels.power = power;
 
         return levels;
+    }
+
+    /*
+   * This method scales the joystick input so for low joystick values, the
+   * scaled value is less than linear.  This is to make it easier to drive
+   * the arm more precisely at slower speeds.
+   */
+    static double scaleInput(double dVal)  {
+        double[] raiseArmScale = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        double[] lowerArmScale = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+        if (index < 0) {
+            index = -index;
+        } else if (index > 16) {
+            index = 16;
+        }
+
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -lowerArmScale[index];
+        } else {
+            dScale = raiseArmScale[index];
+        }
+
+        return dScale;
     }
 }

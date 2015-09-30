6235f5c220d13f54779bdaf0a2b54bfe2aa1f39e
4924_Users <nrrobotics@gmail.com>
2015-09-30 13:20:30 -0400

    Registered Sir Hammer teleop so we could test it with driver station

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -64,6 +64,7 @@ public class FtcOpModeRegister implements OpModeRegister {
     //manager.register ("PushBotManual", PushBotManual.class);
     manager.register ("SensorTest", SensorTest.class);
     manager.register ("BobsPracticeBaseTankDrive", BobsPracticeBaseTankDrive.class);
+    manager.register ("SirHammerTeleop", SirHammerTeleop.class);
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.

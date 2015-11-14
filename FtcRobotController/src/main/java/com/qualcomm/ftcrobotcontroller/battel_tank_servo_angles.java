e6a208c2e33077717aa61e27cc858bb6e46094c2
FTC4924 <nrrobotics@gmail.com>
2015-11-14 10:59:06 -0500

    Alexis' changes from Friday session

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/battel_tank_servo_angles.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/battel_tank_servo_angles.java
@@ -6,6 +6,9 @@ package com.qualcomm.ftcrobotcontroller;
 public class battel_tank_servo_angles {
     public float servo1;
     public float servo2;
+    public float servo3;
+    public float servo4;
+    public float servo5;
 
     public battel_tank_servo_angles() {
     }

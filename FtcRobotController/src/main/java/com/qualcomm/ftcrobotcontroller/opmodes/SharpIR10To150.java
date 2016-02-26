1128bb093755d8ff7a62695162c4a1070d3932fe
William <wtipton@spectrumsedge.com>
2016-02-25 22:09:58 -0500

    Finished sharp IR sensor driver

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SharpIR10To150.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SharpIR10To150.java
@@ -1,33 +1,38 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsUsbDeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.AnalogInputController;
+import com.qualcomm.robotcore.hardware.I2cController;
 
 /**
  * Created by 4924_Users on 2/19/2016.
  */
 public class SharpIR10To150 extends SharpIRSensor {
 
-    private final ModernRoboticsUsbDeviceInterfaceModule USB_MODULE;
-    private final int MODULE_PORT;
-    private final int MINIMUM_DISTANCE = 10;
-    private final int MAXIMUM_DISTANCE = 150;
-    private final double CENTIMETER_CONVERSION_RATE = 1023 / (MAXIMUM_DISTANCE - MINIMUM_DISTANCE);
+    private AnalogInput input;
+    private final int MINIMUM_DISTANCE_CM = 5;
+    private final int MAXIMUM_DISTANCE_CM = 30;
+    private final int MINIMUM_DISTANCE = 90;
+    private final int MAXIMUM_DISTANCE = 880;
+    private final double CENTIMETER_CONVERSION_RATE = (MAXIMUM_DISTANCE - MINIMUM_DISTANCE) / (MAXIMUM_DISTANCE_CM - MINIMUM_DISTANCE_CM);
 
-    public SharpIR10To150(ModernRoboticsUsbDeviceInterfaceModule deviceInterfaceModule, int physicalPort) {
+    public SharpIR10To150(AnalogInput analogInput) {
 
-        USB_MODULE = deviceInterfaceModule;
-        MODULE_PORT = physicalPort;
+        input = analogInput;
     }
 
     @Override
     public double getDistance() {
 
-        return (double) MINIMUM_DISTANCE + this.USB_MODULE.getAnalogInputValue(this.MODULE_PORT) / CENTIMETER_CONVERSION_RATE;
+        int reversedScaleDistance = MAXIMUM_DISTANCE - (input.getValue() - MINIMUM_DISTANCE);
+
+        return (double) reversedScaleDistance / CENTIMETER_CONVERSION_RATE;
     }
 
     @Override
     public int getRawDistance() {
 
-        return this.USB_MODULE.getAnalogInputValue(this.MODULE_PORT);
+        return input.getValue();
     }
 }
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SharpIRSensor.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SharpIRSensor.java
@@ -1,33 +1,17 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.AnalogInputController;
 import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.I2cController;
+import com.qualcomm.robotcore.hardware.I2cDevice;
 
 /**
  * Created by 4924_Users on 2/19/2016.
  */
-public abstract class SharpIRSensor implements HardwareDevice {
+public abstract class SharpIRSensor {
 
     public abstract double getDistance();
 
     public abstract int getRawDistance();
-
-    @Override
-    public String getDeviceName() {
-        return null;
-    }
-
-    @Override
-    public String getConnectionInfo() {
-        return null;
-    }
-
-    @Override
-    public int getVersion() {
-        return 0;
-    }
-
-    @Override
-    public void close() {
-
-    }
 }
